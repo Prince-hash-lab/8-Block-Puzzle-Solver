2762d91a401260e488b34e303c04dce46830cc07
Prince-hash-lab <76578997+Prince-hash-lab@users.noreply.github.com>
2021-05-09 12:37:48 +0530

    ne

--- simple.cpp
+++ simple.cpp
@@ -1,9 +1,54 @@
-#include<bits/stdc++.h>
-#include <glib.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{   gchar *str = "A hindi character: अ\n"; 
-	setlocale(LC_ALL, "en_US.utf8"); 
-	g_print(str); 
-    return 0;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+int maxWidth(Node *root){
+   
+    if (root ==NULL) return 0;
+    int count=1,res=1;
+	cout<<" ------"<<endl;
+    queue <Node* > q;
+    q.push(root);q.push(NULL);
+    while(!q.empty() && q.front()!=NULL)
+    {
+	cout<<" --[][][][][-"<<endl;
+        count=0;
+        while(q.front()!=NULL)
+        {
+	        cout<<" @#@#@#@#@#@#@#$#%$"<<endl;
+			cout<<" count is "<<count<<endl;
+            Node* curr =q.front(); q.pop();
+            if(curr->left!=NULL) { q.push(curr->left);count++;}
+            if(curr->right!=NULL) { q.push(curr->right);count++;}
+            
+        }
+        q.push(NULL);
+        q.pop();
+        res=max(count, res);
+		cout<< " res is "<< res<<endl;
+    }
+    return res;
+}  
+
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	root->left->right=new Node(50);
+	root->right->left=new Node(60);
+	
+	cout<<maxWidth(root);
+	return 0;
 }
