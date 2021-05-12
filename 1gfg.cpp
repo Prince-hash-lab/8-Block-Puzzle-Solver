e44a0b0264d6d131498ee2c70afa1c47ef3bc8af
Prince-hash-lab <76578997+Prince-hash-lab@users.noreply.github.com>
2021-05-12 11:16:31 +0530

    gfg edited

--- 1gfg.cpp
+++ 1gfg.cpp
@@ -1,32 +1,73 @@
-// { Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-void printPat(int n);
+// // { Driver Code Starts
+// #include<bits/stdc++.h>
+// using namespace std;
+// void printPat(int n);
 
+// int main()
+// {
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+// 	int n;
+// 	cin>>n;
+//     printPat(n);
+//     cout<<endl;
+// 	}
+//     return 0;
+// }// } Driver Code Ends
+
+
+// /*You are required to complete this method*/
+// void printPat(int n)
+// {
+//     for(int i=n;i>0;i--)
+//     {
+//         for(int j=n;j>0;j--)
+//             for(int k=i;k>0;k--)
+//                 cout<<j<<" ";
+//         cout<<"$";
+//     }
+// //Your code here
+// }
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+string to_upper(string str);
+ 
 int main()
 {
 	int t;
 	cin>>t;
 	while(t--)
 	{
-	int n;
-	cin>>n;
-    printPat(n);
-    cout<<endl;
+		string str1;
+		cin>>str1;
+		cout << to_upper(str1)<<endl;
 	}
-    return 0;
+	return 0;
 }// } Driver Code Ends
 
 
-/*You are required to complete this method*/
-void printPat(int n)
-{
-    for(int i=n;i>0;i--)
-    {
-        for(int j=n;j>0;j--)
-            for(int k=i;k>0;k--)
-                cout<<j<<" ";
-        cout<<"$";
+
+string to_upper(string str){
+    //code
+    // for ( int i=0;i<str.length();i++)
+    // {  int a=str[i];
+    //   int b=a-32;
+    //   char c=b;
+    //     str[i]=c;
+        
+    //   cout<<a<<" "<<b<<" "<<c<<" "<<str[i]<<endl;
+    // }
+    // return str;
+    for ( int i=0;i<str.length();i++)
+    {  int a=str[i];
+      int b=a-32;
+      char c=b;
+        str[i]=c;
     }
-//Your code here
+    return str;
 }
