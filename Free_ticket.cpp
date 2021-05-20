f73e4afa30b8eea8f95d9a5546bc64ec085fb064
Prince-hash-lab <76578997+Prince-hash-lab@users.noreply.github.com>
2021-05-20 12:18:00 +0530

    next

--- Free_ticket.cpp
+++ Free_ticket.cpp
@@ -1,7 +1,22 @@
+// {
+// 	// Place your snippets for cpp here. Each snippet is defined under a snippet name and has a prefix, body and 
+// 	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+// 	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the 
+// 	// same ids are connected.
+// 	// Example:
+// 	// "Print to console": {
+// 	// 	"prefix": "log",
+// 	// 	"body": [
+// 	// 		"console.log('$1');",
+// 	// 		"$2"
+// 	// 	],
+// 	// 	"description": "Log output to console"
+// 	// }
+// }
 
 #include<bits/stdc++.h>
 #include<stdint.h>
-#define N 105
+// #define N 105
 using namespace std;
 void printArray(vector<int> arr, int size)
 {
@@ -25,7 +40,7 @@ for (int i=0; i<size ; i++)
 #define pq              priority_queue
 #define pqb             priority_queue<int>
 #define pqs             priority_queue<int,vi,greater<int>>
-#define flop(i,a,b)     for(int i=a;i<b;i++)           
+#define fo(i,a,b)     for(int i=a;i<b;i++)           
 #define setbits(x)      _buildin_popcountll(x)
 #define zrobits(x)      _buildin_ctzll(x)
 #define mod             1000000007
@@ -36,38 +51,19 @@ for (int i=0; i<size ; i++)
 #define FIO             ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0)
 // mt19937                 rng(chrono:: steady_clock::now().time_since_epoch().count());
 
+void addedge(vector<int> adj[],int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
 int32_t main()
 {
-    // int arr[10]={1,2,5,10,20,50,100,200,500,2000};
-    // sort(arr,arr+10, greater<int>() );
-    // int n=54;
-    // int count=0;
-    // int i=9;
-    // while(n!=0)
-    // {   if(i==-1) break;
-    //     if(arr[i]<=n)
-    //     {
-    //         count++;
-    //         n-=arr[i];
-    //         continue;
-    //     }
-    //     i--;
-    // }
-    int n; 
-    cin>>n;
-    vi a(n);
-    flop(i,0,n)  cin>>a[i];
-    int x;
-    cin>>x;
-    sort(a.begin(),a.end(),greater<int>());
-    int ans=0;
-    flop(i,0,n)
-    {
-        ans+=x/a[i];
-        x-=(x/a[i])*a[i];
-    }
-    cout<<ans<<" ";
-
-    return 0;
+	FIO;
+	int v,e;
+    cin>>v>>e;
+    vpii adj[v];
+    fo()
+    
+	return 0;
 }
-//C:\Users\my pc\Documents\c1codeforces
