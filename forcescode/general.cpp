ee1220e8ca96dfeb438eca9c6b7ce5cb028f2fbd
Prince-hash-lab <76578997+Prince-hash-lab@users.noreply.github.com>
2021-07-10 22:11:56 +0530

    further more

--- forcescode/general.cpp
+++ forcescode/general.cpp
@@ -31,10 +31,13 @@ int32_t main()
     FIO;
     w(t)
     {
+        
         int n;cin>>n;
         int x;
+
         for(int k=2;k<=35;++k)
         {
+
             int den=pow(2,k)-1;
             if(n%den)  continue;
             x=n/den;
